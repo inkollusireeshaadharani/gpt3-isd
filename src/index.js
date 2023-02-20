6512f0be16ff7165dd426e1f94aeebd912df0d96
Dharani Inkollu <inkolludharani2508@gmail.com>
2023-02-20 12:55:06 +0530

    "Commit1"

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
+// helps in hooking the react file to html
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+//we still haven't created this app
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<App/>,document.getElementById('root'));
+//this root id is nothing but the id of a div from the index.html file
+//create App.js in src folder
+
+//line 9 simply means that we are going to render the App component inside the div with id root
